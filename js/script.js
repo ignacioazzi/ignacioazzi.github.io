074bc35b1bdfb173b81f8bb1194d5eef0444f174
ignacioazzi <ignacioazzi@gmail.com>
2017-01-27 15:33:46 -0200

    Upload portfolio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+	$(document).ready(function(){
+		$('.slider').slick({
+			dots: true,
+		});
+	});
+
+
+	$(".slider").click(function() {
+		$("nav").addClass("nav-hidden").removeClass("nav-visible");
+    $('html, body').animate({
+        scrollTop: $(".slider").offset().top * 1
+    }, 600);
+});
+
+
+$(window).bind('mousewheel', function(event) {
+            if (event.originalEvent.wheelDelta >= 0) {
+             $("nav").addClass("nav-visible").removeClass("nav-hidden");
+           }
+           else {
+            $("nav").addClass("nav-hidden").removeClass("nav-visible");
+          }
+        });
