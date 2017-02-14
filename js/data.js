ececb62fe7e340eec97c47189eece841c6e56698
ignacioazzi <ignacioazzi@gmail.com>
2017-02-14 03:22:31 -0300

    LOTS of changes....
    Changed old versions for v3. Implemented global navbar with menu. CSS is
    still a big mess. Need to clean up style.css

---
+++ js/data.js
@@ -0,0 +1,76 @@
+
+var $projects = $("#projects-list"),
+projectsCode = $("#projects-js").html(),
+projectsCompile = Handlebars.compile(projectsCode),
+projectsData = {
+  "projects": [
+  
+  {
+    "company": "TeamTask",
+    "company-description":"Task management for small projects made simple in the form of an  Android app.",
+    "project-description":"Developer console for an ecosystem of modular smartphones, a tool by which developers register and manage modules.",
+    "url":"teamtask.html",
+    "tooltip":"Pick me!",
+    "bg_img":"img/covers/covers-05.png"},
+    {
+      "company": "Educabilia",
+      "company-description":"Biggest e-learning platform in Latin America selling online and presential courses",
+      "project-description":"Developer console for an ecosystem of modular smartphones, a tool by which developers register and manage modules.",
+      "url":"educabilia.html",
+      "tooltip":"Pick me!",
+      "bg_img":"img/covers/covers-04.png"},
+      {"company": "Project ARA",
+      "company-description":"Developer console for an ecosystem of modular smartphones, a tool by which developers register and manage modules.",
+      "project-description":"Firstly, to generate an expert review to mark pain points of the existing UI. Next, to propose solutions to create a more standardized, user-friendly platform.",
+      "url":"ara.html",
+      "tooltip":"Pick me!",
+      "bg_img":"img/covers/covers-02.png"},
+
+      {
+        "company": "AutoSave",
+        "company-description":"Research and  business model ideation to encourage millennials to improve their saving habits through the aid of gaming.",
+        "project-description":"To interview gamers users to decide whether there was a feasible business model where millennials would be encouraged to save and invest. Then, based on the info collected, to help ideate that product",
+        "url":"autosave.html",
+        "tooltip":"Pick me!",
+        "bg_img":"img/covers/covers-01.png"},
+        
+
+        {
+          "company": "Presidio National Park",
+          "company-description":"National Park in San Francisco, California who was looking to completely redesign its website.",
+          "project-description":"Redesign main sections include home page, landing pages, detail pages. Design from scratch a map section.",
+          "url":"presidio.html",
+          "tooltip":"Pick me!",
+          "bg_img":"img/covers/covers-03.jpg"},
+          ]
+        }
+        $projects.html(projectsCompile(projectsData));
+
+
+        var $process = $("#process"),
+        handlebarsTemplate = $("#handlebars-template").html(),
+        templateCompile = Handlebars.compile(handlebarsTemplate),
+        data = {
+          "sections": [{
+            "title": "Meet clients and users",
+            "subtitle": "Who are am I designining for?",
+            "text":"Customer journeys, user flows, value propositions, personas and wireframes are more than just documents. They are a fundamental part of a working with the client and as a part of a team.<br>I am always committed to create meaningful documents that tells what and why I am taking a design decisions. The use of a clear graphic and written language is crucial to explain something to somebody who has never seen it before."},
+            {
+              "title": "Research before start",
+              "subtitle": "Think, plan, do.",
+              "text":"Customer journeys, user flows, value propositions, personas and wireframes are more than just documents. They are a fundamental part of a working with the client and as a part of a team.<br>I am always committed to create meaningful documents that tells what and why I am taking a design decisions. The use of a clear graphic and written language is crucial to explain something to somebody who has never seen it before."},
+              {
+                "title": "Create meaningful documents.",
+                "subtitle": "Deliverables meant to be read and used.",
+                "text": "Customer journeys, user flows, value propositions, personas and wireframes are more than just documents. They are a fundamental part of a working with the client and as a part of a team.<br>I am always committed to create meaningful documents that tells what and why I am taking a design decisions. The use of a clear graphic and written language is crucial to explain something to somebody who has never seen it before."
+              },
+              {
+                "title": "Test, question yourself.",
+                "subtitle": "We are not the users.",
+                "text":"Not always you can reach your users to test your hypothesis, but we you can squeeze that opportunity as much as possible. I have been part of user testing and interviews in the past check AutoSave project and currently, getting into the data driven design world"
+              }]
+            }
+            $process.html(templateCompile(data));
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
-// 	$(document).ready(function(){
-// 		$('.slider').slick({
-// 			dots: true,
-//       adaptiveHeight: true
-//       	});
-// 	});
+var navbar = '  <nav id=\"site-navigation\" class=\"main-navigation nav-visible\" role=\"navigation\"><div class=\"container nav-bar\">\r\n    <div class=\"row\">\r\n      <div class=\"module left\">\r\n        <a href=\"index.html\">\r\n          <p class=\"site-title\"> <b>Ignacio Azzi<\/b>\r\n            UX Designer\r\n          <\/p>\r\n        <\/a>\r\n      <\/div>\r\n      <div class=\"module right\">\r\n        <span style=\"position:relatve\" class=\"dropdown\" href=\"#\">Projects\r\n          <ul class=\"dropdown_items\">\r\n           <li><a href=\"teamtask.html\">TeamTask<\/a><\/li>\r\n       <li><a href=\"educabilia.html\">Educabilia<\/a><\/li>\r\n    <li><a href=\"ara.html\">Project Ara<\/a><\/li>\r\n                <li><a href=\"lan.html\">LAN<\/a><\/li>\r\n  <li><a href=\"presidio_old.html\">Presidio National Park<\/a><\/li>\r\n            <li><a href=\"autosave.html\">AutoSave<\/a><\/li>\r\n                               <\/ul>\r\n        <\/span>\r\n\r\n        <a target=\"_blank\" class=\"\" href=\"Ignacio_Azzi-Resume.pdf\">Resume<\/a>\r\n        <a href=\"#contact\">Contact me<\/a>\r\n      <\/div>\r\n    <\/div>\r\n  <\/div>\r\n<\/nav>';
 
 
+$("body").prepend(navbar);
 
-// $(".slider").click(function() {
-// 		$("nav").addClass("nav-hidden").removeClass("nav-visible");
-//     $('html, body').animate({
-//         scrollTop: $(".slider").offset().top * 1
-//     }, 600);
-// });
-
+var name = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+$("a[href$='" + name + "']").parent().addClass('active_menu_item');
 
 $(window).bind('mousewheel', function(event) {
-            if (event.originalEvent.wheelDelta >= 0) {
-             $("nav").addClass("nav-visible").removeClass("nav-hidden");
-           }
-           else {
-            $("nav").addClass("nav-hidden").removeClass("nav-visible");
-          }
-        });
+	if (event.originalEvent.wheelDelta >= 0) {
+		$("nav").addClass("nav-visible").removeClass("nav-hidden");
+	}
+	else {
+		$("nav").addClass("nav-hidden").removeClass("nav-visible");
+	}
+});
+
+
+
