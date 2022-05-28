082f49feb295fb57daf79001f94832fa312a2f4b
ignacioazzi <ignacioazzi@gmail.com>
2022-05-28 14:55:10 +0200

    More updates...

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
-var navbar = '<nav id=\"site-navigation\" class=\"main-navigation nav-visible\" role=\"navigation\"><div class=\"container nav-bar\">\r\n\t\t<div class=\"row\">\r\n\t\t\t<div class=\"module left active_menu_item\">\r\n\t\t\t\t<a href=\"index.html\">\r\n\t\t\t\t\t<p class=\"site-title\"> <b>Ignacio Azzi<\/b>\r\n\t\t\t\t\t\tProduct Designer\r\n\t\t\t\t\t<\/p>\r\n\t\t\t\t<\/a>\r\n\t\t\t<\/div>\r\n\t\t\t<div class=\"module right\">\r\n\t\t\t<div class=\"responsive_menu\">\r\n\t\t\t\t<span class=\"ham_icon visible_xs hidden-sm hidden-md hidden-lg\">Menu<\/span>\r\n\t\t\t\t<span class=\"menu_content hidden-xs\">\r\n\t\t\t\t\t\t\t<span style=\"position:relatve\" class=\"dropdown\" href=\"#\">Projects\r\n\t\t\t\t\t<ul class=\"dropdown_items\">\r\n\t\t\t\t\t\t<li><a href=\"teamtask.html\">TeamTask<\/a><\/li>\r\n\t\t\t\t\t\t<li><a href=\"educabilia.html\">Educabilia<\/a><\/li>\r\n\t\t\t\t\t\t<li><a href=\"ara.html\">Project Ara<\/a><\/li>\r\n\t\t\t\t\t\t<li><a href=\"lan.html\">LAN<\/a><\/li>\r\n\t\t\t\t\t\t<li><a href=\"presidio_old.html\">Presidio National Park<\/a><\/li>\r\n\t\t\t\t\t\t<li><a href=\"autosave.html\">AutoSave<\/a><\/li>\r\n\t\t\t\t\t<\/ul>\r\n\t\t\t\t<\/span>\r\n\r\n\t\t\t\t<a href=\"mailto:ignacioazzi@gmail.com\">Contact me<\/a>\r\n\t\t\t\t<\/span>\r\n\t\t\t<\/div>\r\n\t\t\t<div>\r\n\t\t<\/div>\r\n\t\t<\/div>\r\n\t<\/nav>';
+var navbar =`
+<nav id="site-navigation" class="main-navigation nav-visible" role="navigation"><div class="container nav-bar">
+<div class="row">
+<div class="module left active_menu_item">
+<a href="index.html">
+<p class="site-title">
+Ignacio Azzi · 
+<b>Product Designer</b>
+</p>
+</a>
+</div>
+<div class="module right">
+<div class="responsive_menu">
+<span class="ham_icon visible_xs hidden-sm hidden-md hidden-lg">Menu</span>
+<span class="menu_content hidden-xs">
+<span style="position:relatve" class="dropdown fwb" href="#">Projects
+<ul class="dropdown_items">
+<li><a href="teamtask.html">TeamTask</a></li>
+<li><a href="educabilia.html">Educabilia</a></li>
+<li><a href="ara.html">Project Ara</a></li>
+<li><a href="lan.html">LAN</a></li>
+<li><a href="presidio_old.html">Presidio National Park</a></li>
+<li><a href="autosave.html">AutoSave</a></li>
+</ul>
+</span>
+
+<a class="fwb" href="mailto:ignacioazzi@gmail.com">Contact me</a>
+</span>
+</div>
+<div>
+</div>
+</div>
+</div></div></nav>`
+
 
 
 $("body").prepend(navbar);
